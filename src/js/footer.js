f67ae9d4f6a471dec36b65d57bf5c651ee63d1d3
Melvin Carvalho <melvincarvalho@gmail.com>
2016-05-06 19:57:43 +0200

    load storage

--- src/js/footer.js
+++ src/js/footer.js
@@ -4,7 +4,7 @@ const Footer = React.createClass({
   render() {
     return (
       <footer className="credits">
-        Shorcuts: Ctrl+L = login | Ctrl+S = save <br/>
+        Shorcuts: Ctrl+L = login | Ctrl+S = save | Ctrl+O = load <br/>
         Made with <span className="heart"></span> by <a href="https://github.com/afonsopacifer" className="link" target="_blank">@afonsopacifer</a> and by <a href="https://github.com/melvincarvalho" className="link" target="_blank">@melvincarvalho</a>
       </footer>
     )

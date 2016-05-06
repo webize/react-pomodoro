a270587315a33d4d5275dd930298a628cc3c2bbc
Melvin Carvalho <melvincarvalho@gmail.com>
2016-05-06 13:33:33 +0200

    Add keys

--- src/js/footer.js
+++ src/js/footer.js
@@ -4,7 +4,8 @@ const Footer = React.createClass({
   render() {
     return (
       <footer className="credits">
-        Made with <span className="heart"></span> by <a href="https://github.com/afonsopacifer" className="link" target="_blank">@afonsopacifer</a>
+        Shorcuts: ctrl+L = login<br/>
+        Made with <span className="heart"></span> by <a href="https://github.com/afonsopacifer" className="link" target="_blank">@afonsopacifer</a> and by <a href="https://github.com/melvincarvalho" className="link" target="_blank">@melvincarvalho</a>
       </footer>
     )
   }

a72a6b7debd9ea4bacc6090eab5bf64ab0ee323d
Melvin Carvalho <melvincarvalho@gmail.com>
2016-05-06 17:27:08 +0200

    Save

--- src/js/footer.js
+++ src/js/footer.js
@@ -4,7 +4,7 @@ const Footer = React.createClass({
   render() {
     return (
       <footer className="credits">
-        Shorcuts: ctrl+L = login<br/>
+        Shorcuts: Ctrl+L = login | Ctrl+S = save <br/>
         Made with <span className="heart"></span> by <a href="https://github.com/afonsopacifer" className="link" target="_blank">@afonsopacifer</a> and by <a href="https://github.com/melvincarvalho" className="link" target="_blank">@melvincarvalho</a>
       </footer>
     )
